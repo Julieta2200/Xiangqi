6b257d75f480dcb54e745844ab1a91154f3ec7d8
Julieta2200 <julieta@megista.io>
2025-02-06 22:39:07 +0400

    add capture of figure

--- Projects/Tutorial_1/tutorial_1.gd
+++ Projects/Tutorial_1/tutorial_1.gd
@@ -17,9 +17,13 @@ func create_soldier():
 			"team": Board.team.Red
 		}
 	}
-	
 	%Board.create_state(state)
 	soldier = %Board.state[Vector2(5, 3)]
+	
+func soldier_movement_dialog():
+	%Dialog.appear("Before crossing the river, soldier can only move 1 position forward each step.")
+	soldier.arrows.get_child(0).visible = true
+	
 	
 func create_dummy_figure_for_soldier():
 	delete_figure()
@@ -36,24 +40,7 @@ func create_dummy_figure_for_soldier():
 	
 	%Board.create_state(state)
 	%Board.generate_save_state()
-
-func create_dummy_figure_for_general():
-	delete_figure()
-	state = {
-		Vector2(4, 2): {
-			"type": Figure.Types.Soldier,
-			"team": Board.team.Black
-		},
-		Vector2(3, 0): {
-			"type": Figure.Types.General,
-			"team": Board.team.Red
-		}
-		
-	}
 	
-	%Board.create_state(state)
-	%Board.generate_save_state()
-
 func create_general():
 	delete_figure()
 	state = {
@@ -67,7 +54,7 @@ func create_general():
 	general =  %Board.state[Vector2(4, 0)]
 	for i in general.arrows.get_children():
 		i.visible = true
-
+		
 func create_advisor_for_general():
 	delete_figure()
 	state = {
@@ -84,38 +71,6 @@ func create_advisor_for_general():
 	%Board.create_state(state)
 	%Board.generate_save_state()
 	general =  %Board.state[Vector2(5, 0)]
-	
-func create_soldier_for_advisor():
-	delete_figure()
-	state = {
-		Vector2(3, 2): {
-			"type": Figure.Types.Soldier,
-			"team": Board.team.Black
-		},
-		Vector2(5,2): {
-			"type": Figure.Types.Advisor,
-			"team": Board.team.Red,
-		}
-	}
-	
-	%Board.create_state(state)
-	%Board.generate_save_state()
-
-
-func create_adviser():
-	delete_figure()
-	state = {
-		Vector2(4,1): {
-			"type": Figure.Types.Advisor,
-			"team": Board.team.Red,
-		}
-	}
-	%Board.create_state(state)
-	%Board.generate_save_state()
-	advisor =  %Board.state[Vector2(4, 1)]
-	for i in advisor.arrows.get_children():
-		i.visible = true
-
 
 func create_black_general():
 	delete_figure()
@@ -137,10 +92,96 @@ func create_black_general():
 	%Board.create_state(state)
 	%Board.generate_save_state()
 
+func create_dummy_figure_for_general():
+	delete_figure()
+	state = {
+		Vector2(4, 2): {
+			"type": Figure.Types.Soldier,
+			"team": Board.team.Black
+		},
+		Vector2(3, 0): {
+			"type": Figure.Types.General,
+			"team": Board.team.Red
+		}
+	}
+	
+	%Board.create_state(state)
+	%Board.generate_save_state()
+	
+func create_figures_for_general():
+	delete_figure()
+	state = {
+		Vector2(5,1): {
+			"type": Figure.Types.General,
+			"team": Board.team.Red
+		},
+		Vector2(3,5): {
+			"type": Figure.Types.Soldier,
+			"team": Board.team.Red,
+		},
+		Vector2(4,9): {
+			"type": Figure.Types.General,
+			"team": Board.team.Black
+		},
+		Vector2(4,1): {
+			"type": Figure.Types.Soldier,
+			"team": Board.team.Black,
+		}
+	}
+	%Board.create_state(state)
+	%Board.generate_save_state()
+	
+func create_adviser():
+	delete_figure()
+	state = {
+		Vector2(4,1): {
+			"type": Figure.Types.Advisor,
+			"team": Board.team.Red,
+		}
+	}
+	%Board.create_state(state)
+	%Board.generate_save_state()
+	advisor =  %Board.state[Vector2(4, 1)]
+	for i in advisor.arrows.get_children():
+		i.visible = true
 
-func soldier_movement_dialog():
-	%Dialog.appear("Before crossing the river, soldier can only move 1 position forward each step.")
-	soldier.arrows.get_child(0).visible = true
+func create_soldier_for_advisor():
+	delete_figure()
+	state = {
+		Vector2(3, 2): {
+			"type": Figure.Types.Soldier,
+			"team": Board.team.Black
+		},
+		Vector2(5,2): {
+			"type": Figure.Types.Advisor,
+			"team": Board.team.Red,
+		}
+	}
+	
+	%Board.create_state(state)
+	%Board.generate_save_state()
+
+
+func create_sildiers_for_advisor():
+	delete_figure()
+	state = {
+		Vector2(3, 0): {
+			"type": Figure.Types.Soldier,
+			"team": Board.team.Black
+		},
+		Vector2(1, 2): {
+			"type": Figure.Types.Soldier,
+			"team": Board.team.Black
+		},
+		Vector2(3,2): {
+			"type": Figure.Types.Advisor,
+			"team": Board.team.Red,
+		}
+	}
+	%Board.create_state(state)
+	%Board.generate_save_state()
+	
+
 
 func delete_figure():
 	%Board.unhighlight_markers()
@@ -216,34 +257,61 @@ func computer_move():
 				%Dialog.appear("Capture the black soldier in 3 moves.")
 				%Board.markers[Vector2(3, 2)].highlighted_spot.visible = false
 		18:
-			if !%Board.state.has(Vector2(4,2)) and %Board.state[Vector2(4,2)]:
+			if !(%Board.state.has(Vector2(4,2)) and %Board.state[Vector2(4,2)].type == Figure.Types.General):
 				delete_figure()
 				%Dialog.appear("Capture the black soldier in 3 moves.")
 				%Board.load_move(15)
+				return
+			else:
+				create_figures_for_general()
+				%Dialog.appear("How to capture the black soldier with the help of the red soldier in 2 moves?.")
+		19:
+			if %Board.state.has(Vector2(4,1)) and %Board.state[Vector2(4,1)].type == Figure.Types.General:
+				delete_figure()
+				%Dialog.appear("Two generals can't be placed on the same file without any pieces in between.")
+				%Board.load_move(18)
+				return
+		20:
+			if !(%Board.state.has(Vector2(4,1)) && %Board.state[Vector2(4,1)].type == Figure.Types.General):
+				delete_figure()
+				%Dialog.appear("How to capture the black soldier with the help of the red soldier in 2 moves?.")
+				%Board.load_move(18)
 				return
 			else:
 				await get_tree().create_timer(1).timeout
 				create_adviser()
 				%Board.markers[Vector2(5, 0)].highlighted_spot.visible = true
 				%Dialog.appear("Move the advisor to the highlighted spot.")
-		19:
+		21:
 			if !%Board.state.has(Vector2(5,0)):
 				%Dialog.appear("Move the advisor to the highlighted spot.")
 				delete_figure()
-				%Board.load_move(18)
+				%Board.load_move(20)
 				return
 			else:
 				await get_tree().create_timer(1).timeout
 				create_soldier_for_advisor()
 				%Board.markers[Vector2(5, 0)].highlighted_spot.visible = false
 				%Dialog.appear("Capture the black soldier in 2 moves.")
-		21:
+		23:
 			if !(%Board.state.has(Vector2(3,2)) && %Board.state[Vector2(3,2)].type == Figure.Types.Advisor):
 				%Dialog.appear("Capture the black soldier in 2 moves.")
 				delete_figure()
-				%Board.load_move(19)
+				%Board.load_move(21)
 				return
 			else:
 				await get_tree().create_timer(1).timeout
-				print("end")
+				create_sildiers_for_advisor()
+				%Dialog.appear("Advisor can't leave the palace.Which soldier can it capture in 2 moves?.")
+		25:
+			if !(%Board.state.has(Vector2(3,0)) && %Board.state[Vector2(3,0)].type == Figure.Types.Advisor):
+				%Dialog.appear("Which soldier can it capture in 2 moves?.")
 				delete_figure()
+				%Board.load_move(23)
+				return
+			else:
+				await get_tree().create_timer(1).timeout
+				delete_figure()
+				%Dialog.disappear()
+				
+			
