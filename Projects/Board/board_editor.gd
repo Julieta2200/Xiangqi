28204ed2794afb49dfcfae6f19bd7f9610186081
Julieta2200 <julieta@megista.io>
2025-02-20 15:09:18 +0400

    markers

--- Projects/Board/board_editor.gd
+++ Projects/Board/board_editor.gd
@@ -1,11 +1,25 @@
 extends Node2D
 
+var state : Dictionary
+const board_rows = 10
+const board_cols = 9
+var markers : Dictionary
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	initialize_markers()
 
+func _on_marker_editor_selected_marker(marker : MarkerEditor):
+	if %Garrison.selected_figure != null:
+		if !state.has(marker.board_position):
+			state[marker.board_position] = {
+				"type": %Garrison.selected_figure.type,
+				"team": Board.team.Red
+			}
+		%Garrison.selected_figure = null
+		
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func initialize_markers():
+	for i in range(board_rows):
+		for j in range(board_cols):
+			markers[Vector2(j,i)] = $Markers.get_child(i).get_child(j)
+			markers[Vector2(j,i)].board_position = Vector2(j,i)
