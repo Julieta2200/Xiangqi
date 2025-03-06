14a66ea067a2f43776752b0591ba6718ab187bea
Grisha Kirakosyan <grisha.kirakosyan@verve.com>
2025-03-06 21:07:44 +0400

    clean up dialog

--- Projects/Dialog/dialog.gd
+++ Projects/Dialog/dialog.gd
@@ -1,15 +1,9 @@
 class_name Dialog extends Control
 
-func appear(text: String, time: float = 0):
+func appear(text: String):
 	visible = true
 	$Panel/RichTextLabel.text = text
 	$AnimationPlayer.play("dialog")
-	if time != 0:
-		$Timer.start()
 
 func disappear():
 	visible = false
-
-
-func _on_timer_timeout() -> void:
-	disappear()
