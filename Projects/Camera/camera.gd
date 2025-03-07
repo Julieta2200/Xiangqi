0dbd4375f79d1debc91fba3c0a56f0f9aaf14228
Julieta2200 <julieta@megista.io>
2025-03-07 15:24:18 +0400

    camera limit

--- Projects/Camera/camera.gd
+++ Projects/Camera/camera.gd
@@ -1,15 +1,27 @@
 extends Camera2D
 
-@export var zoom_speed: float = 1
+@export var zoom_speed: float = 3
 @export var move_speed: float = 1000.0
 var zoom_min: float = 0.65
 var zoom_max: float = 2.275
-var move_right_max:float = 1800
-var move_left_max: float = 380
-var move_up_max: float = 447
-var move_down_max: float = 1600
+var viewport_size : Vector2
+#var move_right_max:float = 1800
+#var move_left_max: float = 380
+#var move_up_max: float = 447
+#var move_down_max: float = 1600
+var move_right_max:float
+var move_left_max: float 
+var move_up_max: float
+var move_down_max: float
 
 var locked: bool = false
+
+func _ready() -> void:
+	viewport_size = get_viewport().size
+	move_right_max = limit_right - viewport_size.x / 2
+	move_left_max = limit_left + viewport_size.x / 2
+	move_down_max = limit_bottom - viewport_size.y / 2
+	move_up_max = limit_top + viewport_size.y / 2
 
 func _process(delta):
 	if locked:
@@ -26,7 +38,7 @@ func camera_zoom(delta):
 	if Input.is_action_just_pressed("zoom_out"):
 		zoom_target = zoom - Vector2(0.15,0.15)
 		zoom_target = zoom_target.max(Vector2(zoom_min,zoom_min))
-
+	
 	zoom = zoom.move_toward(zoom_target, zoom_speed * delta)
 	
 	
