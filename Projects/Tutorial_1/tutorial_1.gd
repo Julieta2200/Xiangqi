70ded445dd13a0d47246d9134a8ca05a71577f1b
Julieta2200 <julieta@megista.io>
2025-03-17 19:47:34 +0400

    dialog next

--- Projects/Tutorial_1/tutorial_1.gd
+++ Projects/Tutorial_1/tutorial_1.gd
@@ -29,42 +29,43 @@ func _ready():
 	%Board.create_state(initial_state)
 
 func camera_zoom():
-	var text1: TextBlock = TextBlock.new("You can scroll to zoom in and out to have a better view of surroundings (If mouse scroll doesn't work please use Q,E keys, sorry we're still developing :) )",
-	 "Name", "Sprite")
-	var text2: TextBlock = TextBlock.new("inchvor ban",
-	 "Name", "Sprite")
-	var texts: Array[TextBlock]
-	texts.append(text1)
-	texts.append(text2)
-	%Dialog.appear(texts,
-	camera_movement)
+	var texts: Array[TextBlock] 
+	texts = [TextBlock.new("You can scroll to zoom in and out to have a better view of surroundings (If mouse scroll doesn't work please use Q,E keys, sorry we're still developing :) )","Name", "Sprite")]
+	%Dialog.appear(texts,camera_movement)
 
 func camera_movement():
-	print("test")
-	return
-	%Dialog.appear("Use W, A, S, D to look around.",look_out)
+	var texts: Array[TextBlock] 
+	texts = [TextBlock.new("Use W, A, S, D to look around.","Name", "Sprite")]
+	%Dialog.appear(texts,look_out)
 
 func look_out():
-	%Dialog.appear("Look! An enemy soldier is approaching.")
+	var texts: Array[TextBlock] 
+	texts = [TextBlock.new("Look! An enemy soldier is approaching.","Name", "Sprite")]
 	$Camera/AnimationPlayer.play("enemy_spawn")
 
 func soldier_spawn():
 	%Board.set_figure(Figure.Types.Soldier, Vector2(4,7), "Cloud", Board.team.Black)
-	%Dialog.appear("Don’t go any further!!!",spawn_garrison)
+	var texts: Array[TextBlock] 
+	texts = [TextBlock.new("Don’t go any further!!!","Name", "Sprite")]
+	%Dialog.appear(texts,spawn_garrison)
 	await get_tree().create_timer(3).timeout
 	$Camera/AnimationPlayer.play("RESET")
 
 func spawn_garrison():
-	%Dialog.appear("We have a few soldiers with us, but you need to summon them.")
-	await get_tree().create_timer(3).timeout
+	var texts: Array[TextBlock] 
+	texts = [TextBlock.new("We have a few soldiers with us, but you need to summon them.","Name", "Sprite"),
+			TextBlock.new("To summon a soldier you need to have enough energy.","Name", "Sprite"),
+			TextBlock.new("To get more energy you need to capture enemy soldiers.","Name", "Sprite")]
+	%Dialog.appear(texts,explain_pawn_card)
+	await get_tree().create_timer(4).timeout
 	%PowerMeter.show_energy_bar()
-	%Dialog.appear("To summon a soldier you need to have enough energy.")
-	await get_tree().create_timer(3).timeout
-	%Dialog.appear("To get more energy you need to capture enemy soldiers.")
+
 	
 func explain_pawn_card():
 	%Garrison.show()
-	%Dialog.appear("Click on the pawn card to summon it.")
+	var texts: Array[TextBlock] 
+	texts = [TextBlock.new("Click on the pawn card to summon it.","Name", "Sprite")]
+	%Dialog.appear(texts)
 	%Garrison.get_soldier_card().highlight()
 	await get_tree().create_timer(3).timeout
 	%Garrison.get_soldier_card().unhighlight()
@@ -74,26 +75,23 @@ func _on_garrison_card_selected(selected_card: FigureCard) -> void:
 		summon_soldier()
 
 func summon_soldier():
-	%Dialog.appear("The distance meter shows how far in the arena you can summon your soldiers.")
-	%PowerMeter.show_distance_bar()
-	await get_tree().create_timer(3).timeout
-	%Dialog.appear("The more soldiers you have the longer the distance. You can’t summon a soldier inside the palace.")
-	await get_tree().create_timer(3).timeout
-	%Dialog.appear("Click on one of the markers to summon your soldier there.")
-
+	var texts: Array[TextBlock] 
+	texts = [TextBlock.new("The distance meter shows how far in the arena you can summon your soldiers.","Name", "Sprite"),
+			TextBlock.new("The more soldiers you have the longer the distance. You can’t summon a soldier inside the palace.","Name", "Sprite"),
+			TextBlock.new("Click on one of the markers to summon your soldier there.","Name", "Sprite")]
+	%Dialog.appear(texts)
 
 func _on_board_set_figure(marker: BoardMarker) -> void:
 	if first_time:
 		move_and_capture_enemy()
 
 func move_and_capture_enemy() -> void:
-	%Dialog.appear("Now it’s our turn to move.")
-	await get_tree().create_timer(3).timeout
-	%Dialog.appear("Click on a pawn to see all possible moves.")
-	await get_tree().create_timer(3).timeout
-	%Dialog.appear("Pawns can move only forward, by one step, once they cross the river, they can also move one step horizontally.")
-	await get_tree().create_timer(3).timeout
-	%Dialog.appear("Capture the enemy pawn.")
+	var texts: Array[TextBlock] 
+	texts = [TextBlock.new("Now it’s our turn to move.","Name", "Sprite"),
+			TextBlock.new("Click on a pawn to see all possible moves.","Name", "Sprite"),
+			TextBlock.new("Pawns can move only forward, by one step, once they cross the river, they can also move one step horizontally.","Name", "Sprite"),
+			TextBlock.new("Capture the enemy pawn.","Name", "Sprite"),]
+	%Dialog.appear(texts)
 	first_time = false
 
 
@@ -101,23 +99,24 @@ func check_status():
 	var soldier = %Board.get_figures(Board.team.Red, Figure.Types.Soldier)
 	var enemy_soldier = %Board.get_figures(Board.team.Black, Figure.Types.Soldier)
 	if soldier.size() == 0:
-		%Dialog.appear("You let the enemy pawn to destroy our guard…")
-		await get_tree().create_timer(3).timeout
-		reset()
+		var texts: Array[TextBlock] = [TextBlock.new("You let the enemy pawn to destroy our guard…","Name", "Sprite")]
+		%Dialog.appear(texts,reset)
 		return
 		
 	if enemy_soldier.size() == 0:
-		%Dialog.appear("You won, that's it for today :) ")
+		var texts: Array[TextBlock] = [TextBlock.new("You won, that's it for today :) ","Name", "Sprite")]
+		%Dialog.appear(texts)
 		return
 	
 	if soldier[0].board_position.y > enemy_soldier[0].board_position.y:
-		%Dialog.appear("You let the enemy pawn to pass our guard…")
-		await get_tree().create_timer(3).timeout
-		reset()
+		var texts: Array[TextBlock] = [TextBlock.new("You let the enemy pawn to pass our guard…","Name", "Sprite")]
+		%Dialog.appear(texts,reset)
+		return
 
 
 func reset():
-	%Dialog.appear("Please, try again.")
+	var texts: Array[TextBlock] = [TextBlock.new("Please, try again.","Name", "Sprite")]
+	%Dialog.appear(texts)
 	var new_state: Dictionary = initial_state.duplicate()
 	new_state[Vector2(4,7)] = {
 		"type": Figure.Types.Soldier,
