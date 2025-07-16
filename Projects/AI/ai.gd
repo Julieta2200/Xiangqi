ae0f2e80a07bcd5db9ba6fcac7278087dafe267a
Grisha Kirakosyan <grisha.kirakosyan@verve.com>
2025-07-16 13:37:00 +0400

    AI minimax

--- Projects/AI/ai.gd
+++ Projects/AI/ai.gd
@@ -9,18 +9,81 @@ func get_figures() -> Array[FigureComponent]:
 	return board.get_figures(BoardV2.Teams.Black)
 	
 func make_move() -> bool:
-	var figures: Array[FigureComponent] = get_figures()
-	var possible_moves: Array[Dictionary] = []
-	for figure in figures:
-		var moves: Array[Vector2i] = figure.chess_component.get_moves()
+	select_best_move(3)
+	return true
+
+func evaluate_position(state: Dictionary, team: BoardV2.Teams) -> int:
+	var score: int = 0
+	for pos in state:
+		var figure: FigureComponent = state[pos]
+		if figure.chess_component.team == team:
+			score += figure.chess_component.value
+		else:
+			score -= figure.chess_component.value
+	return score
+
+func duplicate_state(state: Dictionary) -> Dictionary:
+	var d: Dictionary
+	for pos in state:
+		d[pos] = state[pos]
+	return d
+
+func simulate_move(state: Dictionary, move: Dictionary) -> Dictionary:
+	var new_state: Dictionary = duplicate_state(state)
+	new_state[move["end"]] = new_state[move["start"]]
+	new_state.erase(move["start"])
+	return new_state
+
+func minimax(state: Dictionary, depth: int, maximizingPlayer: bool, alpha: int, beta: int) -> int:
+	if depth == 0:
+		return evaluate_position(state, BoardV2.Teams.Black)
+	
+	if maximizingPlayer:
+		var maxEval = -INF
+		for move in get_all_legal_moves(BoardV2.Teams.Black, state):
+			var new_board = simulate_move(state, move)
+			var eval = minimax(new_board, depth - 1, false, alpha, beta)
+			maxEval = max(maxEval, eval)
+			alpha = max(alpha, eval)
+			if beta <= alpha:
+				break
+		return maxEval
+	else:
+		var minEval = INF
+		for move in get_all_legal_moves(BoardV2.Teams.Red, state):
+			var new_board = simulate_move(state, move)
+			var eval = minimax(new_board, depth - 1, true, alpha, beta)
+			minEval = min(minEval, eval)
+			beta = min(beta, eval)
+			if beta <= alpha:
+				break
+		return minEval
+
+func get_all_legal_moves(team: BoardV2.Teams, state: Dictionary) -> Array[Dictionary]:
+	var legal_moves: Array[Dictionary] = []
+	for pos in state:
+		var figure: FigureComponent = state[pos]
+		if figure.chess_component.team != team:
+			continue
+		var moves: Array[Vector2i] = figure.chess_component.calculate_moves(state, pos)
 		for move in moves:
-			possible_moves.append({
-				"start": figure.chess_component.position,
+			legal_moves.append({
+				"start": pos,
 				"end": move
 			})
-	if possible_moves.size() == 0:
-		return false
-	
-	var move: Dictionary = possible_moves[randi() % possible_moves.size()]
-	board.move_figure_AI(move)
-	return true
+		
+	return legal_moves
+
+
+func select_best_move(depth: int) -> void:
+	var best_score = -INF
+	var best_move = null
+	var moves = get_all_legal_moves(BoardV2.Teams.Black, board.state)
+	for move in moves:
+		var new_board = simulate_move(board.state, move)
+		var score = minimax(new_board, depth - 1, false, -INF, INF)
+		if score > best_score:
+			best_score = score
+			best_move = move
+	if best_move:
+		board.move_figure_AI(best_move)
