2394a566baab7cdbc709a55b6ae5781ebaa6d251
Julieta2200 <julieta@megista.io>
2025-07-21 18:04:10 +0400

    general move animation

---
+++ Projects/Figure/Components/MoveComponents/Magma/general_move_component.gd
@@ -0,0 +1,18 @@
+extends MoveComponent
+
+
+func move_animation(old_pos: Vector2, new_pos: Vector2) -> void:
+	var direction = old_pos - new_pos
+	
+	if direction.y > 0:
+		animated_sprite.play("move_back")
+	elif direction.y < 0:
+		animated_sprite.play("move_front")
+	elif direction.x > 0:
+		animated_sprite.play("move_left")
+	else:
+		animated_sprite.play("move_right")
+
+func generate_move_tween(target_pos):
+	await get_tree().create_timer(0.5).timeout
+	super.generate_run_tween(target_pos)
