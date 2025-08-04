0839f4f9a546a3428784a913480c952646336d62
Grisha Kirakosyan <grisha.kirakosyan@verve.com>
2025-08-04 17:20:20 +0400

    Freeze counter

--- Projects/Ui/support.gd
+++ Projects/Ui/support.gd
@@ -5,6 +5,16 @@ extends Control
 signal freeze(chance: float)
 signal wall()
 
+var freeze_active: bool = true
+var freeze_counter: int : 
+	set(fc):
+		if fc < 0:
+			fc = 0
+		freeze_counter = fc
+		freeze_active = freeze_counter == 0
+
+@export var freeze_counter_limit: int = 2
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -18,11 +28,15 @@ func _process(delta: float) -> void:
 func _on_freeze_gui_input(event: InputEvent) -> void:
 	if !event.is_action_pressed("click"):
 		return
-	
+	if !freeze_active:
+		return
+	freeze_counter = freeze_counter_limit
 	emit_signal("freeze", freeze_chance)
 	
 func activate(result: bool) -> void:
 	visible = result
+	if visible:
+		freeze_counter -= 1
 
 
 func _on_wall_gui_input(event: InputEvent) -> void:
