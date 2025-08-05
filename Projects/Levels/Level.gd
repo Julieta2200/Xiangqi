38d17f47d28f2b683e66b3dea30bece2215087d7
Julieta2200 <julieta@megista.io>
2025-08-05 17:47:03 +0400

    decision

--- Projects/Levels/Level.gd
+++ Projects/Levels/Level.gd
@@ -1,19 +1,24 @@
 class_name Level extends Node2D
 
-
+@export var has_decision: bool
 @onready var board: BoardV2 = %Board
+@onready var gameplay_ui: GameplayUI = $GameplayUI
+
 
 func _ready() -> void:
 	board.game_over.connect(_on_board_game_over)
-	$GameplayUI.power_meter.energy_depleted.connect(game_over_energy_depleted)
+	gameplay_ui.power_meter.energy_depleted.connect(game_over_energy_depleted)
 
 
 func game_over_energy_depleted():
 	load_main_scene()
 
 func _on_board_game_over(win):
-	print(win)
-	load_main_scene()
+	if win:
+		if has_decision:
+			gameplay_ui.decision_activate()
+	else:
+		get_tree().reload_current_scene()
 
 func load_main_scene():
 	get_tree().change_scene_to_file("res://Projects/Levels/Overworld/overworld.tscn")
