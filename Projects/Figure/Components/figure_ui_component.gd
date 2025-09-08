d9412008d291639acca50747a31bc210428e96ef
Grisha Kirakosyan <grisha.kirakosyan@verve.com>
2025-09-08 15:12:49 +0400

    Highlight

--- Projects/Figure/Components/figure_ui_component.gd
+++ Projects/Figure/Components/figure_ui_component.gd
@@ -2,14 +2,25 @@ class_name FigureUIComponent extends Area2D
 
 var active: bool = true
 
+@export var highlight_material: Material
+
 @export var chess_component: ChessComponent
+@export var main_sprite: AnimatedSprite2D
 
 func _on_mouse_entered() -> void:
-	pass
+	if !active:
+		return
+	if main_sprite == null:
+		return
+	main_sprite.material = highlight_material
 
 
 func _on_mouse_exited() -> void:
-	pass
+	if !active:
+		return
+	if main_sprite == null:
+		return
+	main_sprite.material = null
 
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
