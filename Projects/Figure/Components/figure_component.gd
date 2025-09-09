fbf73795f4dc6b672a920ce7f350d25cc7ecdf58
Grisha Kirakosyan <grisha.kirakosyan@verve.com>
2025-09-09 20:36:29 +0400

    cleanup

--- Projects/Figure/Components/figure_component.gd
+++ Projects/Figure/Components/figure_component.gd
@@ -11,21 +11,6 @@ enum Types {SOLDIER, GENERAL, ADVISOR, CHARIOT, HORSE, ELEPHANT, CANNON}
 @export var type: Types
 
 var frozen: bool = false
-@onready var tmp_freeze_scene = load("res://Projects/Support/TmpFreeze.tscn")
-var freeze_obj: Node2D
 
 func delete() -> void:
 	queue_free()
-
-func freeze() -> void:
-	if frozen:
-		return
-	frozen = true
-	freeze_obj = tmp_freeze_scene.instantiate()
-	add_child(freeze_obj)
-
-func unfreeze() -> void:
-	if !frozen:
-		return
-	frozen = false
-	freeze_obj.queue_free()
