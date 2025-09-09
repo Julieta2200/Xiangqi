41ed446e3de4b90915994c946e2bcbfdadadf961
Julieta2200 <julieta@megista.io>
2025-09-10 03:00:20 +0400

    horse move animation

--- Projects/Figure/Components/MoveComponents/Magma/horse_move_component.gd
+++ Projects/Figure/Components/MoveComponents/Magma/horse_move_component.gd
@@ -37,7 +37,6 @@ func _on_figure_animation_finished() -> void:
 	if current_animation.find("move") != -1:
 		if animated_sprite.speed_scale == -1:
 			animated_sprite.speed_scale = 1
-			lava.modulate = Color(1,1,1,0)
 			animated_sprite.play("idle")
 			emit_signal("move_done")
 		else:
@@ -50,15 +49,16 @@ func start_lava_tween():
 	lava.modulate = Color(1,1,1,1)
 	animated_sprite.modulate = Color(1,1,1,0)
 	tween.tween_property(lava, "scale", Vector2(0, 0), 0.5)
-	tween.finished.connect(end_lava_tween)
+	tween.finished.connect(finish_lava_tween)
 
-func end_lava_tween():
+func finish_lava_tween():
 	var tween = create_tween()
 	figure_component.global_position = target_position
 	tween.tween_property(lava, "scale", Vector2(2, 2), 0.5)
-	tween.finished.connect(end_move_anim)
+	tween.finished.connect(finish_move_animation)
 
-func end_move_anim():
+func finish_move_animation():
+	lava.modulate = Color(1,1,1,0)
 	animated_sprite.modulate = Color(1,1,1,1)
 	animated_sprite.speed_scale = -1
 	animated_sprite.frame = animated_sprite.sprite_frames.get_frame_count(animated_sprite.animation) - 1
