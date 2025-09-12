861a4947695518fed736bc8ae64893ab2b32fb09
Julieta2200 <julieta@megista.io>
2025-09-12 21:30:57 +0400

    camera shack

--- Projects/Figure/Components/MoveComponents/Magma/elephant_move_component.gd
+++ Projects/Figure/Components/MoveComponents/Magma/elephant_move_component.gd
@@ -1,9 +1,8 @@
 extends MoveComponent
 
-
 func move_animation(old_pos: Vector2i, new_pos: Vector2i) -> void:
 	var direction = old_pos - new_pos
-	if direction.y > 0:
+	if direction.y > 0 and direction.x < 0:
 		animated_sprite.play("move_front_right")
 	else:
 		animated_sprite.play("idle")
