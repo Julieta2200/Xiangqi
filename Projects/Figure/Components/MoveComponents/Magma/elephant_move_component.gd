739087e17b3541c471d9e586551387d1fecd8004
Julieta2200 <julieta@megista.io>
2025-09-12 18:37:57 +0400

    elephant front right move anim

---
+++ Projects/Figure/Components/MoveComponents/Magma/elephant_move_component.gd
@@ -0,0 +1,13 @@
+extends MoveComponent
+
+
+func move_animation(old_pos: Vector2i, new_pos: Vector2i) -> void:
+	var direction = old_pos - new_pos
+	if direction.y > 0:
+		animated_sprite.play("move_front_right")
+	else:
+		animated_sprite.play("idle")
+
+
+func _on_move_done() -> void:
+	animated_sprite.play("idle")
