ee759b1d9fde8998462365396e259c0e146bbce4
Grisha Kirakosyan <grisha.kirakosyan@verve.com>
2025-09-22 17:33:54 +0400

    Outline bug fix

--- Projects/Figure/Components/figure_ui_component.gd
+++ Projects/Figure/Components/figure_ui_component.gd
@@ -1,17 +1,27 @@
 class_name FigureUIComponent extends Area2D
 
-var active: bool = true
+var active: bool = true : 
+	set(a):
+		active = a
+		if active and mouse_in:
+			shader_component.mouse_entered()
+		else:
+			shader_component.mouse_exited()
+
+var mouse_in: bool = false
 
 @export var chess_component: ChessComponent
 @export var shader_component: ShaderComponenet
 
 func _on_mouse_entered() -> void:
+	mouse_in = true
 	if !active:
 		return
 	shader_component.mouse_entered()
 
 
 func _on_mouse_exited() -> void:
+	mouse_in = false
 	shader_component.mouse_exited()
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
