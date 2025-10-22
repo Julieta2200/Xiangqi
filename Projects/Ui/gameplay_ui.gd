7a605f086c134c6a7849bc5b33200c444b4d67cb
Julieta2200 <julieta@megista.io>
2025-10-22 16:23:54 +0400

    gameplay_ui activate

--- Projects/Ui/gameplay_ui.gd
+++ Projects/Ui/gameplay_ui.gd
@@ -4,10 +4,12 @@ class_name GameplayUI extends CanvasLayer
 @onready var power_meter: PowerMeter = $PowerMeter
 @onready var decision: Control = $Decision
 @onready var card_slots: CardSlots = $CardSlots
+@export var is_active: bool = true
 
 @export var board: BoardV2
 
 func _ready() -> void:
+	update_visibility()
 	card_slots.board = board
 	board.use_special.connect(card_slots.use_special)
 
@@ -17,3 +19,8 @@ func _on_garrison_card_selected(selected_card: FigureCard) -> void:
 
 func decision_activate():
 	decision.show()
+
+func update_visibility():
+	garrison.visible = is_active
+	power_meter.visible = is_active
+	card_slots.visible = is_active
