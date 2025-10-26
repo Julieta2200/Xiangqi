11e94bf1a4098f57889d6b9d50fae57e6dedb9a3
Grisha Kirakosyan <grisha.kirakosyan@verve.com>
2025-10-26 13:36:39 +0400

    Enhance game over conditions and add dialog interactions in bonus_level.gd
    - Introduced new dialog prompts for running out of moves and pawns, improving player feedback during gameplay.
    - Updated game over conditions to trigger specific dialogs before ending the game.
    - Modified bonus_level.tscn to disable AI spawning, ensuring a more controlled gameplay experience.

--- Projects/Levels/Bonus_levels/bonus_level.gd
+++ Projects/Levels/Bonus_levels/bonus_level.gd
@@ -27,8 +27,13 @@ func check_game_over() -> bool:
 	if super.check_game_over():
 		return true
 	if board.move_number > 10:
-		_on_game_over(BoardV2.GameOverResults.Lose, board.move_number)
+		ran_out_of_moves_dialog()
 		return true
+
+	if board.get_figures(BoardV2.Teams.Red).filter(func(f): return f.type == FigureComponent.Types.SOLDIER).size() == 0:
+		ran_out_of_pawns_dialog()
+		return true
+
 	return false
 
 func _enable_play():
@@ -36,12 +41,40 @@ func _enable_play():
 	if GameState.state["first_bonus_introduction"]:
 		blocking_panel.show()
 		run_hint_system()
+	else:
+		attack_dialog()
+
+func attack_dialog() -> void:
+	DialogSystem.start_dialog([
+		DialogSystem.DialogText.new('Attack!', DialogSystem.CHARACTERS.Ashes)], true)
 
 func run_hint_system() -> void:
 	if _hint_index >= hints.size():
 		GameState.state["first_bonus_introduction"] = false
 		GameState.save_game()
 		blocking_panel.hide()
+		attack_dialog()
 		return
 	hints[_hint_index].show()
 	_hint_index += 1
+
+func ran_out_of_pawns_dialog() -> void:
+	DialogSystem.start_dialog([
+		DialogSystem.DialogText.new('We ran out of pawns!', DialogSystem.CHARACTERS.Advisor)], true)
+	DialogSystem.connect("dialog_finished", _ran_out_of_pawns_dialog_finished)
+
+func _ran_out_of_pawns_dialog_finished() -> void:
+	if DialogSystem.is_connected("dialog_finished", _ran_out_of_pawns_dialog_finished):
+		DialogSystem.disconnect("dialog_finished", _ran_out_of_pawns_dialog_finished)
+	_on_game_over(BoardV2.GameOverResults.Lose, board.move_number)
+
+
+func ran_out_of_moves_dialog() -> void:
+	DialogSystem.start_dialog([
+		DialogSystem.DialogText.new('We ran out of time!', DialogSystem.CHARACTERS.Advisor)], true)
+	DialogSystem.connect("dialog_finished", _ran_out_of_moves_dialog_finished)
+
+func _ran_out_of_moves_dialog_finished() -> void:
+	if DialogSystem.is_connected("dialog_finished", _ran_out_of_moves_dialog_finished):
+		DialogSystem.disconnect("dialog_finished", _ran_out_of_moves_dialog_finished)
+	_on_game_over(BoardV2.GameOverResults.Lose, board.move_number)
