e6c9b9090d5731e3375a54283fea18476bff8530
Julieta2200 <julieta@megista.io>
2025-11-21 15:54:28 +0400

    cannon move animation

--- Projects/Figure/Components/MoveComponents/Magma/cannon_move_component.gd
+++ Projects/Figure/Components/MoveComponents/Magma/cannon_move_component.gd
@@ -3,8 +3,7 @@ extends MoveComponent
 var old_pos: Vector2i
 var new_pos: Vector2i
 var target_position: Vector2i
-
-@onready var lava: Sprite2D = $"../lava"
+@onready var shadow_pivot: Node2D = $"../ShadowPivot"
 
 func move_to_position(marker: BoardMarker, initial_position: Vector2i = Vector2i.ZERO) -> void:
 	target_position = marker.global_position
@@ -21,16 +20,12 @@ func move_animation(old_pos: Vector2i, new_pos: Vector2i) -> void:
 		animation += "_back"
 	elif direction.y > 0:
 		animation += "_front"
-
-	match abs(direction.y):
-		1: animation += "_semi"
-		2: animation += "_lite"
-
-	if direction.x > 0:
+	elif direction.x > 0:
 		animation += "_left"
 	elif direction.x < 0:
 		animation += "_right"
 	animated_sprite.play(animation)
+	shadow_pivot.modulate = Color(1,1,1,0)
 
 func _on_figure_animation_finished() -> void:
 	var current_animation = animated_sprite.animation
@@ -38,28 +33,16 @@ func _on_figure_animation_finished() -> void:
 		if animated_sprite.speed_scale == -1:
 			animated_sprite.speed_scale = 1
 			animated_sprite.play("idle")
+			shadow_pivot.modulate = Color(1,1,1,1)
 			call_deferred("emit_signal", "move_done")
 		else:
-			start_lava_tween()
+			finish_move_animation()
 	else:
 		animated_sprite.play("idle")
 
-func start_lava_tween():
-	var tween = create_tween()
-	lava.modulate = Color(1,1,1,1)
-	animated_sprite.modulate = Color(1,1,1,0)
-	tween.tween_property(lava, "scale", Vector2(0, 0), 0.5)
-	tween.finished.connect(finish_lava_tween)
-
-func finish_lava_tween():
-	var tween = create_tween()
-	figure_component.global_position = target_position
-	tween.tween_property(lava, "scale", Vector2(2, 2), 0.5)
-	tween.finished.connect(finish_move_animation)
 
 func finish_move_animation():
-	lava.modulate = Color(1,1,1,0)
-	animated_sprite.modulate = Color(1,1,1,1)
+	figure_component.global_position = target_position
 	animated_sprite.speed_scale = -1
 	animated_sprite.frame = animated_sprite.sprite_frames.get_frame_count(animated_sprite.animation) - 1
 	move_animation(old_pos, new_pos)
