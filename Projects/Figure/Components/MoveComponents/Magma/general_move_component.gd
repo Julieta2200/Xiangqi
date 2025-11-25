a28140bfd48f8715e0f07972629cd15bc0d056dd
Julieta2200 <julieta@megista.io>
2025-11-25 21:32:44 +0400

    fog general animation

--- Projects/Figure/Components/MoveComponents/Magma/general_move_component.gd
+++ Projects/Figure/Components/MoveComponents/Magma/general_move_component.gd
@@ -1,25 +1,21 @@
 extends MoveComponent
 
+var animation : String
 
 func move_animation(old_pos: Vector2i, new_pos: Vector2i) -> void:
 	var direction = old_pos - new_pos
 	
 	if direction.y < 0:
-		if shadow != null:
-			shadow.play("move_back")
-		animated_sprite.play("move_back")
+		animation = "move_back"
 	elif direction.y > 0:
-		if shadow != null:
-			shadow.play("move_front")
-		animated_sprite.play("move_front")
+		animation = "move_front"
 	elif direction.x > 0:
-		if shadow != null:
-			shadow.play("move_left")
-		animated_sprite.play("move_left")
+		animation = "move_left"
 	else:
-		if shadow != null:
-			shadow.play("move_right")
-		animated_sprite.play("move_right")
+		animation = "move_right"
+	if shadow != null:
+		shadow.play(animation)
+	animated_sprite.play(animation)
 
 func generate_move_tween(target_pos):
 	await get_tree().create_timer(0.5).timeout
