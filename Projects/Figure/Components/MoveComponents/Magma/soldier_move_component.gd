ca2aa28135b1fbbf9021ed542f4540ca52fe31c8
Julieta2200 <julieta@megista.io>
2025-11-27 12:25:34 +0400

    advisor disappear animation

--- Projects/Figure/Components/MoveComponents/Magma/soldier_move_component.gd
+++ Projects/Figure/Components/MoveComponents/Magma/soldier_move_component.gd
@@ -34,14 +34,7 @@ func _on_figure_animation_finished() -> void:
 	var current_animation = animated_sprite.animation
 	if current_animation.find("disappear") != -1:
 		figure_component.delete()
-	if shadow != null:
-		shadow.play("idle")
-	animated_sprite.play("idle")
+	super._on_figure_animation_finished()
 
 func disappear(attacker_pos: Vector2i):
-	if attacker_pos == Vector2i(-1,-1):
-		if shadow != null:
-			shadow.play("disappear_back")
-		animated_sprite.play("disappear_back")
-		return
 	disappear_animation(figure_component.chess_component.position,attacker_pos)
