c7eedc41c5d54e855ba1012de79d7d9a002e34c4
Grisha Kirakosyan <grisha.kirakosyan@verve.com>
2025-12-10 18:01:18 +0400

    Add null check for get_tree() in generate_move_tween function to prevent potential errors

--- Projects/Figure/Components/MoveComponents/Magma/general_move_component.gd
+++ Projects/Figure/Components/MoveComponents/Magma/general_move_component.gd
@@ -18,5 +18,7 @@ func move_animation(old_pos: Vector2i, new_pos: Vector2i) -> void:
 	animated_sprite.play(animation)
 
 func generate_move_tween(target_pos):
+	if get_tree() == null:
+		return
 	await get_tree().create_timer(0.5).timeout
 	super.generate_move_tween(target_pos)
