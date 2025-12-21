0301c924f81f5f2ac1445a70c2b4c86bff23058f
Julieta2200 <julieta@megista.io>
2025-12-21 18:14:38 +0400

    continue and new game button

--- Projects/Ui/MainMenu/main_menu.gd
+++ Projects/Ui/MainMenu/main_menu.gd
@@ -16,7 +16,10 @@ var flash_timer: Timer
 ]
 
 var light_original_positions: Array[Vector2] = []
-
+var can_continue: bool:
+	set(c):
+		can_continue = c
+		$CanvasLayer/Navigation/Continue.disabled = !can_continue
 
 @onready var ui_animation: AnimationPlayer = %UIAnimation
 @onready var nav_blocker: Panel = %NavBlocker
@@ -25,6 +28,7 @@ var light_original_positions: Array[Vector2] = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	can_continue = GameState.save_exists()
 	canvas_modulate.color = default_color
 	_start_flash_timer()
 	_start_light_floating()
@@ -98,9 +102,15 @@ func _on_new_game_pressed() -> void:
 	ui_animation.play("disappear")
 	nav_blocker.visible = true
 	main_animation.play("start")
+	GameState.new_game()
 
 func play_ashes_run() -> void:
 	ashes.play("run")
 
 func open_overworld() -> void:
 	SceneManager.change_scene(SceneManager.Scenes.Overworld)
+
+func _on_continue_pressed() -> void:
+	ui_animation.play("disappear")
+	nav_blocker.visible = true
+	main_animation.play("start")
