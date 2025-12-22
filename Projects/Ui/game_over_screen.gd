eadaa1eaddc0c0461c6fa2e6e4861a2e4bfcedcd
Julieta2200 <julieta@megista.io>
2025-12-22 18:58:54 +0400

    text

--- Projects/Ui/game_over_screen.gd
+++ Projects/Ui/game_over_screen.gd
@@ -2,6 +2,13 @@ class_name GameOverScreen extends Control
 
 
 @onready var audio_player: AudioStreamPlayer = $AudioStreamPlayer
+@onready var retry_button: Button = $Buttons/Retry
+@onready var quit_button: Button = $Buttons/Quit
+
+func _ready() -> void:
+	# Set translated text for all menu buttons
+	retry_button.text = tr("RETRY")
+	quit_button.text = tr("QUIT")
 
 func _on_retry_pressed() -> void:
 	get_tree().reload_current_scene()
