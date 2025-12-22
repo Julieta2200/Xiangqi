1573163e5df734f6cc08836fb5a003b3dea0bba6
Julieta2200 <julieta@megista.io>
2025-12-22 14:40:22 +0400

    music tween

--- Projects/Ui/PauseMenu/pause_menu.gd
+++ Projects/Ui/PauseMenu/pause_menu.gd
@@ -1,12 +1,23 @@
 extends Control
 
+@onready var audio_player: AudioStreamPlayer = $AudioStreamPlayer
 
 func _ready() -> void:
-	$BlurOverlay.material.set_shader_parameter("blur_amount", 1)
+	$BlurOverlay.material.set_shader_parameter("blur_amount", 2)
 
 
 func _on_visibility_changed() -> void:
+	if audio_player == null:
+		return
+		
 	if visible:
-		$AudioStreamPlayer.play()
+		play_music()
 	else:
-		$AudioStreamPlayer.stop()
+		audio_player.stop()
+
+func play_music():
+	audio_player.volume_db = -20
+	audio_player.play()
+
+	var tween:= create_tween()
+	tween.tween_property(audio_player, "volume_db", 1, 1)
