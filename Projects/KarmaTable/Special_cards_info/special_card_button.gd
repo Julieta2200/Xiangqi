78b16ee8814b62513772e9f68013c0b7a3bf6283
Julieta2200 <julieta@megista.io>
2025-12-26 21:27:35 +0400

    card slot

--- Projects/KarmaTable/Special_cards_info/special_card_button.gd
+++ Projects/KarmaTable/Special_cards_info/special_card_button.gd
@@ -8,12 +8,11 @@ signal card_select(type,number)
 @export var active : bool:
 	set(a):
 		active = a
-		#disabled = !active
 		$name_icon.visible = !active
 
 func _ready() -> void:
 	if type == SpecialCardInfo.SpecialType.NONE:
-		change_button_to_image()
+		change_button_image()
 		active = false
 	if type == SpecialCardInfo.SpecialType.TREE_TRUNK or type == SpecialCardInfo.SpecialType.SNAKE_CHAIN \
 	or type == SpecialCardInfo.SpecialType.DISCONNECTION_MIST:
@@ -30,23 +29,11 @@ func _ready() -> void:
 	$icon.texture = image
 
 func _on_special_card_pressed() -> void:
-	print("click")
 	emit_signal("card_select", type, number)
 
-func change_button_to_image():
-	# 1. Ստեղծում ենք նոր StyleBoxTexture
+func change_button_image():
 	var new_style = StyleBoxTexture.new()
-	
-	# 2. Բեռնում ենք նկարը (Texture)
 	var image = preload("res://Assets/UI/Ability info/Ability Slot(Locked).png")
-	
-	# 3. Նկարը տեղադրում ենք ոճի մեջ
 	new_style.texture = image
-	
-	# (Լրացուցիչ) Եթե ուզում ես, որ նկարը չձգվի սխալ ձևով, կարող ես սահմանել margin-ները (9-patch)
-	# new_style.texture_margin_left = 10
-	# new_style.texture_margin_right = 10
-	
-	# 4. Կիրառում ենք կոճակի "normal" (սովորական) վիճակի վրա
 	add_theme_stylebox_override("normal", new_style)
 	add_theme_stylebox_override("hover", new_style)
